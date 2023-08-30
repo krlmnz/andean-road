603f92065d4cfb112bc69449142efcd9ff232b8a
Karol Munoz <karol@karolmunoz.com>
2023-08-30 10:36:50 -0400

    Guide to Central Chile

---
+++ docs/config.js
@@ -0,0 +1,74 @@
+'use strict';
+
+// eslint-disable-next-line no-unused-vars
+const config = {
+  style: 'mapbox://styles/kmunoz/cllx1eth201ba01ma7eth4dt8',
+  accessToken:
+    'pk.eyJ1Ijoia211bm96IiwiYSI6ImNsbHdxemd5NTF3Z3Qza3FqZndrc2hjeXgifQ.3UITue1v6QIP9-AISabmag',
+  CSV: './Sample_Data.csv',
+  center: [-71.020537, -33.342419],
+  zoom: 8.6,
+  title: 'Central Chile',
+  description:
+    'Replace with information about your application. Ex. You can search by address to sort the list below by distance. You can also filter the list by language support options, which days a location is open, and whether they have devices to use to complete the survey by phone or online.',
+  sideBarInfo: ['Location_Name', 'Type', 'Google_Maps'],
+  popupInfo: ['Location_Name'],
+  filters: [
+    {
+      type: 'dropdown',
+      title: 'Languages supported: ',
+      columnHeader: 'Languages',
+      listItems: [
+        'Amharic',
+        'ASL',
+        'Cambodian',
+        'Chinese',
+        'Danish',
+        'English',
+        'French',
+        'German',
+        'Greek',
+        'Hindi',
+        'Italian',
+        'Japanese',
+        'Korean',
+        'Language Line Services',
+        'Norwegian',
+        'Oriya',
+        'Portuguese',
+        'Punjabi',
+        'Russian',
+        'Somali',
+        'Spanish',
+        'Swedish',
+        'Tagalog',
+        'Thai',
+        'Tigrinya',
+        'Tongan',
+        'Vietnamese',
+        'Ukranian',
+      ],
+    },
+    {
+      type: 'checkbox',
+      title: 'Devices available: ',
+      columnHeader: 'Devices_available', // Case sensitive - must match spreadsheet entry
+      listItems: ['Computer', 'Wi-Fi', 'Adaptive Laptops'], // Case sensitive - must match spreadsheet entry; This will take up to six inputs but is best used with a maximum of three;
+    },
+    {
+      type: 'dropdown',
+      title: 'Clients: ',
+      columnHeader: 'Clients',
+      listItems: [
+        'Adults',
+        'Disabled',
+        'Homeless',
+        'Immigrants/Refugees',
+        'Low Income',
+        'Seniors',
+        'Youth: Pre-teen',
+        'Youth: Teen',
+      ],
+    },
+  ],
+};s
